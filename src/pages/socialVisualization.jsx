4807ddcaf7032d85f96518590d847cf3e5e45c65
unknown <smriti30jojo@gmail.com>
2024-02-20 10:52:50 +0530

    changes added

--- src/pages/socialVisualization.jsx
+++ src/pages/socialVisualization.jsx
@@ -359,9 +359,9 @@ function divideInputs(openness1, conscientiousness1, extraversion1, agreeablenes
         if(item.postText!==undefined){
             postData.push(item.postText);
         }
-        else{
-          postData.push(item.postLink);
-        }
+        // else{
+        //   postData.push(item.postLink);
+        // }
         // postData.push(item.postText);
      });
      }
