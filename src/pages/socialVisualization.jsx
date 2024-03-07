dfe9e590b5ad951c89bc41915ba3a943527e3b35
smriti-jojo <smriti30jojo@gmail.com>
2024-03-07 15:38:38 +0530

    updated linkedin rapidAPI cred

--- src/pages/socialVisualization.jsx
+++ src/pages/socialVisualization.jsx
@@ -341,6 +341,7 @@ function divideInputs(openness1, conscientiousness1, extraversion1, agreeablenes
       method:'GET',
 
       headers:{
+        // "X-RapidAPI-Key":"f98a7dcf75mshbec3fc4926c0197p1dad9cjsnbf95294b90fa",
         "X-RapidAPI-Key":"f98a7dcf75mshbec3fc4926c0197p1dad9cjsnbf95294b90fa",
       "X-RapidAPI-Host":"linkedin-api8.p.rapidapi.com",
       }
